650f32b568e47dc275be85e447008cd771c730cf
Alexander <Aleksandr.Dedyukhin@vsegda-da.com>
2023-07-28 20:16:11 +0400

    Пофиксил остановку сервера, проблема : при остановке закрывались все приложения java в том числе и дженкинс

--- dist/stop.sh
+++ dist/stop.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-killall LoginServerTask
+# killall LoginServerTask
+kill $(ps aux | grep LoginServerTask | awk '{print $2}')
 echo "Login server is stop"
 
-killall GameServerTask.
+# killall GameServerTask.
+kill $(ps aux | grep GameServerTask. | awk '{print $2}')
 echo "Game server is stop"
 
-killall java
+kill $(ps aux | grep java | grep GameServer.jar | awk '{print $2}')
+kill $(ps aux | grep java | grep LoginServer.jar | awk '{print $2}')
+
 echo "Done"
